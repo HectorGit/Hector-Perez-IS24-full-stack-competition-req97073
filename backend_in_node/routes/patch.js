5831f9b1330f70963514b20b15f549fd8616bd7c
HectorGit <hectorandres.pv@ergonomyx.com>
2023-03-26 19:43:28 -0700

    created pseudocode for patch endpoint to update existing product entru

--- backend_in_node/routes/patch.js
+++ backend_in_node/routes/patch.js
@@ -1,16 +1,36 @@
 const express = require("express");
 const patchrouter = express.Router();
 
-patchrouter.patch("/update_product/:id", (req, res) => 
+patchrouter.patch("/update_product/:product_id", (req, res) => 
     {
- 
-        //pseudocode, read the data, 
-        //find the data with that product id
-        //try to write the new data to the json file
-         
-        var product_id = req.params.id
-        console.log("product id:" , product_id)
-        return res.status(200).send("updated the product !");
+        
+        // product_id = req.params.product_id
+
+        // //PSEUDOCODE
+
+        // // //read in the dummy data (faux-database)
+        // // FileSystem.read
+        // // products = []
+
+        // // find existing data for ID (there will be, since only could've come from the frontend) [on postman, could fail bc ID could not be tied to graphical unit]
+        // product_to_update = products.find(p => p.productId == product_id) //find or filter (will have to test this)
+
+        // // prepare updated project entry by reading the request body
+        // //if there isn't something in the request body, replace it with current data in the product entry
+        // new_project = {
+        //     //'productId':req.body.productId || products[id].productId,
+        //     'productName':req.body.productName || product_to_update.productName,
+        //     'productOwnerName':req.body.productOwnerName || product_to_update.productOwnerName,
+        //     'Developers':req.body.Developers || product_to_update.Developers,
+        //     'scrumMasterName':req.body.scrumMasterName || product_to_update.scrumMasterName,
+        //     'startDate':req.body.startDate || product_to_update.startDate,
+        //     'methodology':req.body.methodology || product_to_update.methodology
+        // }
+
+        // //copy all the existing data(?) and affect the copy w/ the updates
+        // //write the updated data to the file.
+        // // FileSystem.write
+
     }
 );
 
