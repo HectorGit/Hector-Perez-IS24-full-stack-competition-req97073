80b82c4326ab48ddeb89a9aaf70f707fa0a55b86
HectorGit <hectorandres.pv@ergonomyx.com>
2023-03-27 09:56:59 -0700

    experiment with reading and updating content from a json file - will integrate into POST and PATCH endpoints

---
+++ backend_in_node/helpful_snippet.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+console.log("running the js file !")
+
+let rawContent = fs.readFileSync('products_data.json').toString()
+let parsedContent = JSON.parse(rawContent)
+
+console.log(parsedContent)
+
+//do stuff to the content
+updatedContent = parsedContent
+
+fs.writeFileSync('products_data.json', JSON.stringify(updatedContent))
+
