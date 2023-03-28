29ad8eeddf60ee2e58f6a22217eac2ba769faf4d
HectorGit <hectorandres.pv@ergonomyx.com>
2023-03-28 16:30:28 -0700

    cleanup

--- frontend_in_react/web_application_tracker_frontend/src/App.js
+++ frontend_in_react/web_application_tracker_frontend/src/App.js
@@ -1,22 +1,12 @@
 import * as React from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers'; //used for Material UI DateField
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'; //used for Material UI DateField
-
 import { Provider } from 'react-redux'
-
 import { useState, useEffect } from 'react';
 import ProductsPage from './pages/productsPage';
-
 import {store as productStore} from "./redux/productStore"
 
-
-
 function App() {
-
-  // const dispatch = useDispatch()
-
-  //move to inner component?
-  // dispatch(fetchProducts())
 
   return (
     <Provider store={productStore}>
